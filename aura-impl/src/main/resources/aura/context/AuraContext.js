71a5fbe609b399daff2b30b6a32cd11a4db391e0
Kevin Venkiteswaran <kvenkiteswaran@salesforce.com>
2013-04-19 08:26:51 -0700

    Merge changing GVP values into existing map

--- aura-impl/src/main/resources/aura/context/AuraContext.js
+++ aura-impl/src/main/resources/aura/context/AuraContext.js
@@ -272,7 +272,7 @@ AuraContext.prototype.joinGlobalValueProviders = function(gvps, doNotPersist) {
                 this.globalValueProviders[t] = gvp;
             } else {
                 var mergeMap = new MapValue(newGvp["values"]);
-                gvp.merge(mergeMap, false);
+                gvp.merge(mergeMap, true);
             }
 
             if (storage) {
