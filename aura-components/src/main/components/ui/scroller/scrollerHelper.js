13fc21cce36583a7eb4da9f62f785d6939cf3648
dchasman <dchasman@salesforce.com>
2013-09-18 12:13:27 -0700

    prevent throwaway resize and refresh actions on ui.scroller for performance
    @rev goliver@
    @bug W-1851714@

--- aura-components/src/main/components/ui/scroller/scrollerHelper.js
+++ aura-components/src/main/components/ui/scroller/scrollerHelper.js
@@ -29,9 +29,7 @@
         if (!component._refreshTimeout) {
             var that = this;
             component._refreshTimeout = window.setTimeout(function(){
-            	$A.run(function() {
-	            	that._doRefreshScroller(component);
-            	});
+            	that._doRefreshScroller(component);
         	}, 400);
         }
     },
@@ -57,16 +55,10 @@
 			var scroller = component._scroller;
 			if (!$A.util.isUndefined(scroller)) {
 				scroller.unbindTransientHandlers();
-				
 				scroller.refresh();
 								
 				var compEvents = component.getEvent("refreshed");						
 				compEvents.fire();
-				
-				// Goose the x position to insure that onScrollEnd() fires with the correct page fully revealed
-//				if (component.get("v.snap")) {
-//					scroller.scrollTo(scroller.maxScrollX, 0, 0);
-//				}
 			}
 
 			component._refreshing = false;
