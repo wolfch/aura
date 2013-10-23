d5c9c9986bc830b9411b7c8a1a1c60ac8eefd3cd
Derrick <dmcmillen@salesforce.com>
2013-10-23 16:41:10 -0700

    Adding support to turn load more on and off

--- aura-components/src/main/components/ui/scroller/scrollerController.js
+++ aura-components/src/main/components/ui/scroller/scrollerController.js
@@ -31,5 +31,17 @@
     
     scrollBy: function(component, event, helper) {
     	helper.handleScrollBy(component, event);
+    },
+    
+    handleCanShowMoreChange: function (cmp, evt, hlp) {
+    	var newValue = evt.getParam('value').getValue(),
+    		oldValue = cmp.getValue('v.canShowMore').oldValue;
+    	
+    	cmp._canShowMore = newValue; 
+    	
+    	// false => true ?
+    	if (!oldValue && newValue) {
+    		hlp.swapShowMore(cmp);	
+    	}
     }
 })
